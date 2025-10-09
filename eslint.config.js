6e6d9b6bc7f78400af7881c0455153112e1a2fac
wenqx <coderwenn@163.com>
2025-10-09 21:48:30 +0800

    feat: 配置eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
+    parser: "@typescript-eslint/parser", // 如果是 JS，可以省略或使用默认
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@typescript-eslint": tseslint.configs.stylistic,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "react/react-in-jsx-scope": "off", // React 17+ 不需要导入 React
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": "warn",
     },
-  },
-)
+    settings: {
+      react: {
+        version: "detect", // 自动检测 React 版本
+      },
+    },
+  }
+);
